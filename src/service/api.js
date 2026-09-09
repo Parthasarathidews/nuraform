8b6ce1e3946cff361fbd03d9e7f135f8ef853d75
dews parthasarathi <parthasarathi@techdew.com>
2026-09-09 12:55:24 +0530

    updated env variables

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
